bc1da6ee2c3e40462207a50d901e36273a53bda7
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2022-10-21 18:39:04 -0300

    Função 'POST' criada.

--- ex.ts
+++ ex.ts
@@ -19,9 +19,32 @@ class Carro{
 // let data = new Carro("Fiat","Uno","Legal",2010,140000,20000)
 
 Get()
+postData("https://apigenerator.dronahq.com/api/x7BRQ4V1/carros", new Carro("GM","Onix","Sedan",2022,0,65000))
+.then((data) => {
+console.log(data);
+});
 
 function Get() {
     return fetch("https://apigenerator.dronahq.com/api/x7BRQ4V1/carros")
     .then((response) => response.json())
     .then((data) => console.log(data));
 }
+
+async function postData(url = '', data = {}) {
+    const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+    'Content-Type': 'application/json'
+    },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+    body: JSON.stringify(data)
+    });
+    return response.json();
+    }
+    
+
+    
