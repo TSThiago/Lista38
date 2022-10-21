2e8b5249a00b490b19232345ab15d4930456f5f9
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2022-10-21 17:52:53 -0300

    Criação da classe 'Carro'.

---
+++ ex.js
@@ -0,0 +1,11 @@
+var Carro = /** @class */ (function () {
+    function Carro(marca, modelo, categoria, ano, quilometragem, valor) {
+        this.Marca = marca;
+        this.Modelo = modelo;
+        this.Categoria = categoria;
+        this.Ano = ano;
+        this.Quilometragem = quilometragem;
+        this.Valor = valor;
+    }
+    return Carro;
+}());
---
+++ ex.ts
@@ -0,0 +1,17 @@
+class Carro{
+    Marca : string
+    Modelo : string
+    Categoria : string
+    Ano : number
+    Quilometragem : number 
+    Valor : number
+
+    constructor(marca : string , modelo : string, categoria : string, ano : number, quilometragem : number, valor : number){
+        this.Marca = marca
+        this.Modelo = modelo
+        this.Categoria = categoria
+        this.Ano = ano
+        this.Quilometragem = quilometragem
+        this.Valor = valor
+    }
+}
